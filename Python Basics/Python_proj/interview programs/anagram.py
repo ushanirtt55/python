6153c4b10b7bc65daf21ead841555c71f766c2c4
ushanirtt55 <ushanirtt55@gmail.com>
2015-06-24 11:41:43 +0530

    Removed anangram

--- Python Basics/Python_proj/interview programs/anagram.py
+++
@@ -1,5 +0,0 @@
-import itertools
-annograms = lambda word: map(lambda c: ''.join(c),
-                             set(itertools.permutations(word)).intersection(
-                               tuple(w.rstrip()) for w in open('/home/umashankar/Downloads/WORD.LST')))
-print annograms('Test')
